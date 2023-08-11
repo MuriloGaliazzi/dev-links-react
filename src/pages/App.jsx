4a18b9d0796782ba222792d73f9a44f2a3aafdda
galiazzi-17 <murilo.galiazzi@gmail.com>
2023-08-11 10:20:57 -0300

    desenvolvido o profile

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,10 +1,22 @@
-import "./App.css";
+import { useState } from "react";
 import Profile from "../components/profile/profile";
 
-export default function App() {
-    return(
-    <>
-        <Profile>@</Profile>
-    </>
- );
-}
+import "./App.css";
+
+import imgDarkMode from "../img/avatar.jpg"
+import imgLightMode from "../img/Among-Us-feat.jpg"
+
+
+const App = () => {
+    const [isLightMode, setIsLightMode] = useState(false);
+
+    const imgSrc = isLightMode ? imgLightMode : imgDarkMode;
+    const imgAlt = isLightMode ? "Imagem no Modo Claro" : "Imagem no Modo Escuro"
+    return (
+        <div>
+            <Profile imgSrc={imgSrc} imgAlt={imgAlt}>@MuriloGaliazzi</Profile>
+        </div>
+    );
+};
+
+export default App;
