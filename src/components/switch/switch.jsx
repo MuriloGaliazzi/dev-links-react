9747a2453fff286d6aa5279c0f31cd7cbc137221
galiazzi-17 <murilo.galiazzi@gmail.com>
2023-08-11 11:05:49 -0300

    switch button ok

---
+++ src/components/switch/switch.jsx
@@ -0,0 +1,12 @@
+import styles from "./swicth.module.css";
+
+const Swicth =() =>{
+    return(
+        <div id={styles.switch}>
+            <button></button>
+            <span></span>
+        </div>
+    );
+};
+
+export default Swicth;
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Profile from "../components/profile/profile";
+import Swicth from "../components/switch/switch";
 
 import "./App.css";
 
@@ -13,8 +14,9 @@ const App = () => {
     const imgSrc = isLightMode ? imgLightMode : imgDarkMode;
     const imgAlt = isLightMode ? "Imagem no Modo Claro" : "Imagem no Modo Escuro"
     return (
-        <div>
+        <div id="container">
             <Profile imgSrc={imgSrc} imgAlt={imgAlt}>@MuriloGaliazzi</Profile>
+            <Swicth></Swicth>
         </div>
     );
 };
